9a1913321d2e3812b9a3c6f1fd2e5eee6f8f65b6
Frank Chien <frankchien@Franks-Air.attlocal.net>
2021-12-06 11:46:23 -0500

    added roxygen comments

--- R/Sensitivity.R
+++ R/Sensitivity.R
@@ -1,19 +1,14 @@
-GetCounts <- function(pred, truth){
-  #support function that takes vector of predictions and ground truths
-  #allows for either 1 & 0 or T/F vectors
-  #no matter how its passed, we will be using the booleans
-  pred <- as.logical(pred)
-  truth <- as.logical(truth)
-  num_true_pos <- length(which(pred&truth)) 
-  num_true_neg <- length(which(!pred&!truth))
-  num_false_pos <- length(which(pred&!truth))
-  num_false_neg <- length(which(!pred&truth))
-  #returns a vector of 4 digits: num TP, num TN, num FP, num FN
-  return(c(num_true_pos,num_true_neg,num_false_pos,num_false_neg))
-  
-}
+#' gets sensitivity given set of predictions and set of ground truths
+#' predictions and ground truths can either be boolean or 1 and 0, must be same length
+#' 
+#' @param pred vector of 1/0 or True and False representing predictions
+#' @param truth vector of 1/0 or True and False representing gold standards or ground truths
+#' @return  the sensitivity given as true pos/(true pos + false neg)
+#' @examples 
+#' sensitivity(c(T,T,F,T), c(T,T,F,F))
 
 Sensitivity <- function(pred, truth){ #takes either logical vectors or 1 and 0s
   counts <- GetCounts(pred,truth)
   #sensitivity = true pos / (true pos + false neg)
   counts[1] / (counts[1] + counts[4]) #see comments for get counts for indices
+}
--- R/Specificity.R
+++ R/Specificity.R
@@ -1,3 +1,12 @@
+#' gets specificity given set of predictions and set of ground truths
+#' predictions and ground truths can either be boolean or 1 and 0, must be same length
+#'
+#' @param pred vector of 1/0 or True and False representing predictions
+#' @param truth vector of 1/0 or True and False representing gold standards or ground truths
+#' @return  the specificity given as true neg/(true neg + false pos)
+#' @examples
+#' sensitivity(c(T,T,F,T), c(T,T,F,F))
+
 Specificity <-function(pred, truth){
   counts <- GetCounts(pred,truth)
   #specificity = true neg / (true neg + false pos)
