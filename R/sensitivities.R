12eaca8601b18d006cd2074db6e032ff82dd66c9
Frank Chien <frankchien@Franks-MacBook-Air.local>
2021-12-06 14:08:35 -0500

    debugged end bracket

--- R/sensitivities.R
+++ R/sensitivities.R
@@ -1,10 +1,10 @@
 #' gets sensitivity given set of predictions and set of ground truths
 #' predictions and ground truths can either be boolean or 1 and 0, must be same length
-#' 
+#'
 #' @param pred vector of 1/0 or True and False representing predictions
 #' @param truth vector of 1/0 or True and False representing gold standards or ground truths
 #' @return  the sensitivity given as true pos/(true pos + false neg)
-#' @examples 
+#' @examples
 #' sensitivity(c(T,T,F,T), c(T,T,F,F))
 
 sensitivity <- function(pred, truth){ #takes either logical vectors or 1 and 0s
