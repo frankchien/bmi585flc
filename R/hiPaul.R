27492a41738624c2b8f2c5d10335336df08dda8e
Frank Chien <frankchien@Franks-MacBook-Air.local>
2021-12-07 08:09:40 -0500

    hi paul function

---
+++ R/hiPaul.R
@@ -0,0 +1,14 @@
+#' Hi Paul
+#' 
+#' this is a function that just says hi Paul
+#' 
+#' returns the string "hello world, hello Paul. tHis is the most world shattering function ever
+#' 
+#' @examples 
+#' hiPaul()
+#' 
+hiPaul <- function(){
+  print("hello world, hello Paul")
+  x <- "hello world, hello Paul"
+  x
+}
